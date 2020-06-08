4c2f6d5689d21110029b5c5fa5578dc22af82206
margretmwangi <margaret13mwangi@gmail.com>
2020-06-08 20:32:49 +0300

    initialized rateappme

---
+++ rateme/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    profile_photo = models.ImageField(upload_to='profiles/',null=True)
+    bio = models.CharField(max_length=240, null=True)
+    phone = models.PositiveIntegerField(default=0)
---
+++ rateme/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  Profile
+from .serializer import ProfileSerializer
+
+# Create your views here.
+class ProfileList(APIView):
+
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)
+
+
+def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        permission_classes = (IsAdminOrReadOnly,)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
