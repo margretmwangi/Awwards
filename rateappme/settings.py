63c51febda8a912dcd8b84c0b6be02c6fe7cfc83
margretmwangi <margaret13mwangi@gmail.com>
2020-06-08 23:03:00 +0300

    added media folder

--- rateappme/settings.py
+++ rateappme/settings.py
@@ -128,3 +128,6 @@ REST_FRAMEWORK = {
 }
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- rateappme/urls.py
+++ rateappme/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -22,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('rateme.urls')),
     path('api-token-auth/' , obtain_auth_token),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
